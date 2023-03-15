caa32dffe6f98be24886e6259165ac7958458727
VIET HAO <VIETHAO6886@gmail.com>
2023-03-15 10:00:15 +0700

    code

---
+++ WED1043/Lab1/app.js
@@ -0,0 +1,14 @@
+// thông báo mời nhâp a
+let a = prompt("Mời bạn nhập số nguyên a");
+// chuyển kiểu dữ liệu string sang int hệ 10
+a=parseInt(a,10);
+// thông báo mời nhâp b
+let b = prompt("mời bạn nhập số nguyên b");
+// chuyển kiểu dữ liệu string sang int hệ 10
+b=parseInt(b,10);
+// tạo biến c gán = a+b
+var c=a+b;
+// in ra màn hình
+document.write("Tổng = " + c);
+// in ra màn hình console
+console.log(c);
