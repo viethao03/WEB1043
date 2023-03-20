10b957d001ea6fba5994aa47e4c3bf872718153f
VIET HAO <VIETHAO6886@gmail.com>
2023-03-20 11:00:07 +0700

    final

--- WED1043/lap2/bai1/app.js
+++ WED1043/lap2/bai1/app.js
@@ -12,22 +12,19 @@ if (a==0){                              // nếu a = 0 Thì làm lệnh if ==> G
             console.log("Phương trình có vô số nghiệm")
         }else( console.log("phương trình vô nghiệm"))       // ngược lại c != 0 thì làm lệnh elae ==> PT vô nghiệm
 
-    }else(console.log("Phương trình có nghiệm x= ",(-b/c))) // ngược lại a != 0 thì làm lệnh else ==> PT có nghiệm
+    }else{console.log("Phương trình có nghiệm x= ",(-b/c))} // ngược lại a != 0 thì làm lệnh else ==> PT có nghiệm
 
 }else{                                                      // ngược lại a != 0 thì làm lệnh else ==> Giải PT bậc hai
-    console.log("Giải PT bậc 2")
-    let delta = b*b - 4 *a*c                                // Tính delta
-    console.log("Deltal = ",delta)
-    document.write(delta)
-
+    console.log("Giải PT bậc 2");
+    let delta = b*b - 4 *a*c ;                               // Tính delta
+    console.log("Deltal = ",delta);
     if(delta < 0){                                          // nếu delta < 0 thì làm lệnh if ==> PT vô nghiệm
         console.log("Phương trình vô nghiệm")
     }else if(delta == 0){                                   // nếu delta = 0 thì làm lệnh if ==> PT có nghiệm kép
         console.log("Phương trình có nghiệm kép : x = ",(-b/(2*a)) )
-    }else(                                                  // ngược lại 2 điều kiện trên thì làm lệnh else ==> PT có 2 nghiệm
-        console.log("Phương trình có 2 nghiệm phân biệt : "),
-        console.log("x1 = ",(-b + Math.sqrt(delta))/(2*a)),
-        console.log("x2 = ",(-b - Math.sqrt(delta))/(2*a))
-    )
-    
+    }else{                                                // ngược lại 2 điều kiện trên thì làm lệnh else ==> PT có 2 nghiệm
+        console.log("Phương trình có 2 nghiệm phân biệt : ");
+        console.log("x1 = ",(-b + Math.sqrt(delta))/(2*a));
+        console.log("x2 = ",(-b - Math.sqrt(delta))/(2*a));
+    }   
 }
